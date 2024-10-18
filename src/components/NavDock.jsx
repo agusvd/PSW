1c8c6316be6364f46cc092e412cd6d4f39225edb
Agustin Villarroel <azau@MacBook-Pro-de-Agustin.local>
2024-10-18 20:42:44 -0300

    arreglos nuevos

--- src/components/NavDock.jsx
+++ src/components/NavDock.jsx
@@ -2,15 +2,13 @@ import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { useRef, useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Dock, DockIcon } from "./ui/dock";
 
 export default function NavDock() {
     const [isHidden, setIsHidden] = useState(false);
     const { scrollY } = useScroll();
     const lastYRef = useRef(0);
     const location = useLocation();
-    const { t } = useTranslation(); 
+    const { t } = useTranslation();
 
     useMotionValueEvent(scrollY, "change", (y) => {
         const difference = y - lastYRef.current;
@@ -37,19 +35,15 @@ export default function NavDock() {
     ];
 
     return (
-        <motion.div animate={isHidden ? "hidden" : "visible"} whileHover="visible" onFocusCapture={() => setIsHidden(false)} variants={{ hidden: { y: "85%", }, visible: { y: "0%", }, }} transition={{ duration: 0.3 }} className="fixed bottom-0 z-30 flex right-0 left-0 justify-center pt-1 pb-1">
-            <div className="absolute -top-2 left-1/2 -translate-x-1/2 h-1 w-24 bg-gray-300 rounded-full"></div>
-            <TooltipProvider>
-                <Dock direction="middle" className="gap-8 bg-[#134B70]/60 text-[#EEEEEE] *:transition-colors *:duration-300 border-none ">
-                    {menu.map((item) => (
-                        <DockIcon key={item.label} className="hover:*:text-[#134B70]">
-                            <Link to={item.url} className="">
-                                {item.label}
-                            </Link>
-                        </DockIcon>
-                    ))}
-                </Dock>
-            </TooltipProvider>
+        <motion.div animate={isHidden ? "visible" : "hidden"} whileHover="visible" onFocusCapture={() => setIsHidden(false)} variants={{ hidden: { y: "-100%", }, visible: { y: "0%", }, }} transition={{ duration: 0.3 }} className="fixed top-0 z-30 flex right-0 left-0 justify-center pt-1 pb-1">
+            <div className="flex justify-around items-center px-4 py-2 rounded-xl gap-8 bg-[#134B70]/80 h-[50px] text-[#EEEEEE]  border-none">
+                {menu.map((item) => (
+                    <Link to={item.url} className="text-xl hover:text-[#134B70] duration-300 transition-all font-primary w-full text-nowrap">
+                        {item.label}
+                    </Link>
+                ))}
+            </div>
+            <motion.div animate={isHidden ? "hidden" : "hidden"} className="absolute -bottom-2 left-1/2 -translate-x-1/2 h-1 w-24 bg-gray-300 rounded-full"></motion.div>
         </motion.div>
     );
 }
